19cb2e3ed21504ec740f5e8473062ace287b5594
Will C <w.l.couturiaux@gmail.com>
2019-03-17 01:24:02 -0400

    Initial commit

--- PointersLab/main.cpp
+++ PointersLab/main.cpp
@@ -7,9 +7,39 @@
 //
 
 #include <iostream>
+using namespace std;
+
+void passByValue(int);
+void passByRef(int &num1);
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    int num, num1;
+    int* pNum = &num;
+    num1=3;
+    *pNum = 5;
+    passByValue(num1);
+    cout<<"In Main"<<endl;
+    cout<<num1<<endl;
+    passByRef(*pNum);
+    cout<<"In Main"<<endl;
+    cout<<*pNum<<endl;
+    passByValue(*pNum);
+    cout<<"InMain"<<endl;
+    cout<<*pNum<<endl;
+    
+    
     return 0;
+}
+
+void passByValue(int num1){
+    cout<<"In Pass By Value"<<endl;
+    num1++;
+    cout<<num1<<endl;
+}
+
+void passByRef(int &num1){
+    cout<<"In Pass By Ref"<<endl;
+    num1=50;
+    cout<<num1<<endl;
 }
