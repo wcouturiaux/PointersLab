b7adeb3b1ac01fc22762e98cc2b50b09494e1c5f
Will C <w.l.couturiaux@gmail.com>
2019-03-17 00:53:15 -0400

    Initial Commit

---
+++ PointersLab/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PointersLab
+//
+//  Created by William Couturiaux on 3/17/19.
+//  Copyright © 2019 William Couturiaux. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
